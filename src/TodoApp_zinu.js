c317555fc7e7707cf8b3756821151debff838951
hwangj9642 <hwangjj9642@gmail.com>
2022-10-18 11:46:22 +0900

    TodoApp new TodoItem test ok

--- src/TodoApp_zinu.js
+++ src/TodoApp_zinu.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useRef,useCallback } from "react";
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 
@@ -15,8 +15,19 @@ const TodoApp_zinu = () => {
             done: true,
         }
     ]);
+    const nextId = useRef(3);
+    const onInsert = useCallback(text => {
+        setTodos(
+            todos.concat({
+                id: nextId.current,
+                text,
+                done: false,
+            })
+        );
+        nextId.CURRENT += 1;
+    });
     return <div>
-        <TodoForm />
+        <TodoForm onInsert={onInsert}/>
         <TodoList todos={todos}/>
     </div>;
 };
--- src/TodoApp_zinu.test.js
+++ src/TodoApp_zinu.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render, firEvent } from '@testing-library/react';
 import TodoApp_zinu from "./TodoApp_zinu";
+import { fireEvent } from "@testing-library/react/types";
 
 describe('<TodoApp_zinu />', () => {
     it('renders TodoForm TodoList', () => {
@@ -12,5 +13,13 @@ describe('<TodoApp_zinu />', () => {
         const {getByText} = render(<TodoApp_zinu />);
         getByText('TDD 배우기');
         getByText('react-testing-library');
-    })
+    });
+    it('creates new todo', () => {
+        const {getByPlaceholderText, getByText} = render(<TodoApp_zinu />);
+        fireEvent.change(getByPlaceholderText('할 일을 입력하세요'),
+            {target: {value: "새 항목 추가하기"}}
+        );
+        fireEvent.click(getByText("등록"));
+        getByText('새 항목 추가하기');
+    });
 });
