6e906d8ad31bc9898ef91e25756444903f3e6fbf
hwangj9642 <hwangjj9642@gmail.com>
2022-10-18 11:32:23 +0900

    TodoApp TodoForm TodoList render test ok

--- src/TodoApp_zinu.test.js
+++ src/TodoApp_zinu.test.js
@@ -8,4 +8,9 @@ describe('<TodoApp_zinu />', () => {
         getByText('등록'); // TodoForm 화면에 있는지 확인
         getByTestId('TodoList'); // TodoList 화면에 있는지 확인
     });
+    it('renders two defaults todos', () => {
+        const {getByText} = render(<TodoApp_zinu />);
+        getByText('TDD 배우기');
+        getByText('react-testing-library');
+    })
 });
